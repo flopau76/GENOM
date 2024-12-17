0e2e6012053f96e783f6e2becb8363d23f29d1f3
flopau76 <flopau76210@gmail.com>
2024-12-17 16:16:36 +0100

    start adaptation to article db

--- code/load_samples/__main__.py
+++ code/load_samples/__main__.py
@@ -1,13 +1,13 @@
 import load_samples.get_taxid as get_taxid
 from load_samples.ribo_db_build import prepare_ribo_db
 
+import argparse
+
 import sys, os, subprocess
 from typing import Dict
 
 def progressbar(iteration, total, prefix = '', suffix = '', filler = '█', printEnd = "\r") -> None:
-    """
-    Show a progress bar indicating downloading progress
-    """
+    """ Show a progress bar indicating downloading progress """
     percent = f'{round(100 * (iteration / float(total)), 1)}'
 
     add = int(100 * iteration // total)
@@ -18,11 +18,8 @@ def progressbar(iteration, total, prefix = '', suffix = '', filler = '█', prin
         print()
 
 def getter(taxon : str, index : str, ribo_dir : str) -> None:
-    """
-    Run a subprocess to fetch the genome corresponding to the
-    accession number.
-    """
-    test = subprocess.run(
+    """ Run a subprocess to fetch the genome corresponding to the accession number. """
+    subprocess.run(
             f'bio fetch {taxon} -t DNA -f fasta > "{index}/genome_{taxon}.fasta"',
             capture_output=True, shell=True
         )
@@ -32,16 +29,12 @@ def getter(taxon : str, index : str, ribo_dir : str) -> None:
     )
     return 0
 
-def load_taxid(taxon_file : str) -> Dict[str, str]:
-    """
-    Load dictionnary associating a species name with its taxid
-    """
+def load_taxid(taxon_file : str, id=False) -> Dict[str, str]:
+    """ Load dictionnary associating a species name with its taxid """
     return get_taxid.get_id(path=taxon_file)
 
 def generate_report(db_path : str, failed : list) -> None:
-    """
-    Generates the downloading report.
-    """
+    """ Generates the downloading report. """
     with open(os.path.join(db_path, "_failed.txt"), 'w') as fail_file:
         for elt in failed:
             fail_file.write(f"{elt}\n")
@@ -50,27 +43,20 @@ def generate_report(db_path : str, failed : list) -> None:
 
 def main(taxon_file, db, ribo_dir):
     taxon_dico = load_taxid(taxon_file)
-    n = 1
+    n = 0
     e = 0
     failed = []
-
-    os.makedirs(ribo_dir)
-
     print("Starting Genomes Download...\n")
+    progressbar(n, len(taxon_dico))
     for index, taxon in taxon_dico.items():
-
+        n+=1
         enddir = os.path.join(db, f"{index.split('/')[0]}")
-            
         os.makedirs(enddir)
-
         try :
             taxon = get_taxid.get_accession(taxon[0])
             getter(taxon, enddir, ribo_dir)
             progressbar(n, len(taxon_dico))
-
-            n+=1
         except Exception:
-            n+=1
             e+=1
             failed.append(index)
             os.rmdir(enddir)
@@ -84,22 +70,19 @@ def main(taxon_file, db, ribo_dir):
     return 0
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python -m load_sample  <sample_file>")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description="Download genomes from NCBI")
+    parser.add_argument("samples", help="The name of the input file (must be in `input/samples_list/`)")
+    parser.add_argument("--id", help="Set to true if the file contains the accession ids instead of the taxon names", action='store_true')
+    args = parser.parse_args()
 
     root_dir = os.path.abspath(os.path.join(__file__, "..", "..", ".."))
+    taxon_file = args.samples
+    taxon_file = os.path.join(root_dir, "input", "samples_list", taxon_file)
 
-    taxon_file = sys.argv[1]
-    taxon_basename = os.path.basename(taxon_file)
-    if taxon_file == taxon_basename:
-        taxon_file = os.path.join(root_dir, "input", "samples_list", taxon_basename)
-    print(f"Taxon file: {taxon_file}")
-
-    out_dir = os.path.join(root_dir, "input", "sequence_db", taxon_basename.split('.')[0])
+    taxon_basename = os.path.basename(taxon_file).split('.')[0]
+    out_dir = os.path.join(root_dir, "input", "sequence_db", taxon_basename)
+    ribo_dir = os.path.join(root_dir, "input", "ribosome_db", taxon_basename)
     print(f"Output directory: {out_dir}")
-
-    ribo_dir = os.path.join(root_dir, "input", "ribosome_db", taxon_basename.split('.')[0])
 
     if os.path.exists(out_dir):
         print(f"Output directory already exists. If you continue, the content will be erased.")
@@ -110,7 +93,7 @@ if __name__ == '__main__':
         else:
             os.system(f"rm -r {out_dir}")
             os.system(f"rm -r {ribo_dir}")
-
+    os.makedirs(out_dir)
+    os.makedirs(ribo_dir)
 
     main(taxon_file, out_dir, ribo_dir)
-
